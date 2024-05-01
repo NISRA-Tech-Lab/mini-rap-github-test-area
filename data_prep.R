dacf3a1f3cd75a40a23cf1d96499a90cebc8d6a4
jmccallion <120484704+jmccallion@users.noreply.github.com>
2024-05-01 15:04:07 +0100

    add knitr

--- data_prep.R
+++ data_prep.R
@@ -1,9 +1,11 @@
 # uncomment these lines if you need to install packages first.
 # install.packages("tidyverse")
 # install.packages("plotly")
+# install.packages("knitr")
 
 library(tidyverse)
 library(plotly)
+library(knitr)
 
 years <- c(2018, 2019, 2020, 2021, 2022)
 students <- c(145, 157, 161, 126, 163)
