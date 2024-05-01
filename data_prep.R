95a4cbc38604c84b3d021fac8f7b94598203c870
Judith-TechLab <79098174+Judith-TechLab@users.noreply.github.com>
2024-05-01 13:11:03 +0100

    setup files

--- data_prep.R
+++ data_prep.R
@@ -1,1 +1,11 @@
+# uncomment these lines if you need to install packages first.
+# install.packages("tidyverse")
+# install.packages("plotly")
+
 library(tidyverse)
+library(plotly)
+
+years <- c(2018, 2019, 2020, 2021, 2022)
+students <- c(145, 157, 161, 126, 163)
+
+df_fig1 <- data.frame(years, students)
