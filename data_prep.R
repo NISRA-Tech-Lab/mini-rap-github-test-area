7d75e2aab287e9493dee47fbcff9c3c5e51dc7d1
jmccallion <120484704+jmccallion@users.noreply.github.com>
2024-05-02 11:30:32 +0100

    add year 2023

--- data_prep.R
+++ data_prep.R
@@ -6,7 +6,7 @@
 library(plotly)
 library(knitr)
 
-years <- c(2018, 2019, 2020, 2021, 2022)
-students <- c(145, 157, 161, 126, 163)
+years <- c(2018, 2019, 2020, 2021, 2022, 2023)
+students <- c(145, 157, 161, 126, 163, 174)
 
 df_fig1 <- data.frame(years, students)
