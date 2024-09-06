47286bee1bd26b7fb736993c9e58c6a5bb44827d
R0sie0 <146082565+R0sie0@users.noreply.github.com>
2024-09-06 14:38:08 +0100

    Ro'N - title on to Y axis, and Figure heading.

--- data_prep.R
+++ data_prep.R
@@ -1,14 +1,14 @@
 # uncomment these lines if you need to install packages first.
 
-install.packages("plotly")
-install.packages("knitr")
+# install.packages("plotly")
+# install.packages("knitr")
 
 library(plotly)
 library(knitr)
 
 years <- c(2018, 2019, 2020, 2021, 2022, 2023, 2024)
 
-students <- c(100, 100, 161, 157, 163, 451, 800)
+students <- c(100, 100, 161, 157, 163, 451, 240)
 
 
 df_fig1 <- data.frame(years, students)
