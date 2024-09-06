b51ca31789d702f19c67e81fb922de849bca2d38
R0sie0 <146082565+R0sie0@users.noreply.github.com>
2024-09-06 14:21:34 +0100

    Changes made to data_RON

--- data_prep.R
+++ data_prep.R
@@ -8,7 +8,7 @@ library(knitr)
 
 years <- c(2018, 2019, 2020, 2021, 2022, 2023, 2024)
 
-students <- c(145, 457, 161, 157, 163, 451, 800)
+students <- c(100, 100, 161, 157, 163, 451, 800)
 
 
 df_fig1 <- data.frame(years, students)
