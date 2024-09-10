eaace39dbe6633b0e74554329ba394a9b1177a46
Judith-TechLab <79098174+Judith-TechLab@users.noreply.github.com>
2024-09-10 12:14:10 +0100

    use 'diff' values as chart labels

--- data_prep.R
+++ data_prep.R
@@ -8,5 +8,6 @@ library(knitr)
 
 years <- c(2018, 2019, 2020, 2021, 2022, 2023)
 students <- c(145, 157, 161, 157, 163, 401)
+diff <- c(7, 12, 4, -4, 6, 238)
 
-df_fig1 <- data.frame(years, students)
+df_fig1 <- data.frame(years, students, diff)
