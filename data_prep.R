bc1751e5d098278fa9d48d5bf0243a09561474d2
tashadickson <natasha.dickson@finance-ni.gov.uk>
2025-01-06 14:25:15 +0000

    ND Change

--- data_prep.R
+++ data_prep.R
@@ -1,13 +1,12 @@
 # uncomment these lines if you need to install packages first.
 
-# install.packages("plotly")
-# install.packages("knitr")
+
 
 library(plotly)
 library(knitr)
 
 
-years <- c(2018, 2019, 2020, 2021, 2022, 2023, 2024)
+years <- c(2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025)
 students <- c(145, 157, 161, 157, 163, 401, 240)
 diff <- c(7, 12, 4, -4, 6, 238, -161)
 
